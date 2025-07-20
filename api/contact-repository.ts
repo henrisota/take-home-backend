a56c07b9db576f2c74a7316b9352f134193ddc32
Henri Sota <sotahenry@gmail.com>
2025-07-20 14:46:29 +0200

    implement save

--- api/contact-repository.ts
+++ api/contact-repository.ts
@@ -1,17 +1,27 @@
+import { SupabaseClient } from "@supabase/supabase-js";
 import { Contact, ProcessedContact } from "./types";
 
 export interface ContactRepositoryConfiguration {}
 
 export class ContactRepository {
     constructor(
+        private readonly supabase: SupabaseClient,
         private readonly configuration: ContactRepositoryConfiguration
     ) {}
 
     async save(contact: Contact): Promise<ProcessedContact> {
+        const { data, error } = await this.supabase.from('contacts').insert([contact]).select().single();
+
+        if (error) {
+            throw new Error(`Failed to save contact ${contact}`);
+        }
+
         return {
-            ...contact,
-            id: 'id',
-            imported_at: new Date().toISOString(),
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            source: data.source,
+            imported_at: data.imported_at
         } satisfies ProcessedContact;
     }
 }
